05fe3aa788e665fb696152962945faf84c60b6bc
Amber Rouw <78225838+amberrouw@users.noreply.github.com>
2021-11-26 12:09:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log ("Hello Winc Academy");
+
+let name = 'Amber';
+console.log(name);
+
+console.log (4 + 4);
+
+console.log ("4" + "4");
+
+console.log (2 - 2);
+
+console.log (10 / 2);
+        
+let age = 27;
+console.log (typeof age);
+        
+let number = "27";
+console.log (typeof number);
+
+let yourResult = "It worked"
+console.log (yourResult)
