6f009f931a58e7b00c49a12f1957a1efc8242630
Amber Rouw <78225838+amberrouw@users.noreply.github.com>
2021-11-26 12:33:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log ("Hello Winc Academy students");
+/*
+console.log (1230941 * 1823794);
+console.log (637263 / 54);
+*/
+
+//You can't see the results of both sums cause node does not read comments
