6412f949ed56c4c7e095e3a89bf1a88df34332e6
Amber Rouw <78225838+amberrouw@users.noreply.github.com>
2021-12-05 09:19:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const age = 18;
+
+if (age >= 18) {
+
+    console.log("You are welcome inside");
+
+} else {
+
+    console.log("You are to young to come inside");
+
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log("Welcome to the ladies night");
+
+} else {
+
+    console.log("You can't buy a ticket only females are allowed");
+
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+
+    console.log("allowed to drive");
+
+} else {
+
+    console.log("not allowed to drive");
+
+}
