0a093390944d2c1e05472ae5971adb2d8d3b0608
Amber Rouw <78225838+amberrouw@users.noreply.github.com>
2021-12-10 15:44:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//how to make pizza fucntion
+
+const makePizza = function () {
+    console.log("make dough");
+    console.log("heat up the oven on 220°C")
+    console.log("roll out the dough");
+    console.log("put tomatosauce on it");
+    console.log("put toppings on the sauce");
+    console.log("put cheese on top of that");
+    console.log("put the pizza in the oven for 8-10 minutes");
+}
+
+//how to make sushi fucntion
+
+const makeSushi = function () {
+    console.log("place the seaweed on the bamboo mat");
+    console.log("spread some prepared sushi rice over the seaweed");
+    console.log("cut some salmon, avocado and cream cheese");
+    console.log("place the cutted ingredients in the middle of the seaweed with rice");
+    console.log("roll it so you have a nice long sushi");
+    console.log("cut it in pieces with a sharp knife");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
